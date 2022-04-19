1c5ce722b80de8d0a853ca887714e3c48fb5a53f
Ben Young <benyoung32@gmail.com>
2022-04-18 23:25:32 -0700

    format

--- grade_need.c
+++ grade_need.c
@@ -14,17 +14,17 @@ int main () {
 
     printf("Enter the grade you want in the class: ");
     scanf("%s", &gradeWanted);
-    printf("\nEnter the percent you need to get that grade: ");
+    printf("Enter the percent you need to get that grade: ");
     scanf("%lf", &percentNeeded);
-    printf("\nEnter your current percent in the class: ");
+    printf("Enter your current percent in the class: ");
     scanf("%lf", &percentCurrent);
-    printf("\nEnter the weight of the final: ");
+    printf("Enter the weight of the final: ");
     scanf("%lf", &weight);
     //printf("weight: %lf", weight);
 
     double currentPoints = percentCurrent * ((100.0 - weight)/ 100.0);
     double neededPoints = ((percentNeeded - currentPoints) / weight) * 100.0;
-    printf("You need to get at least %.2f%% on the final to get a %s in the class.", neededPoints, gradeWanted);
+    printf("You need to get at least %.2f%% on the final to get a %s in the class.\n", neededPoints, gradeWanted);
     return 0;
 
 }
